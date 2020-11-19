0e636463b8bc01ea45a464ff33ce3afa1420c406
Andre <st165045@stud.uni-stuttgart.de>
2020-11-19 13:34:58 +0100

    advanced metrics + outer unsup loss
    Added preupdate acc as a metric to see inner update performance.
    Added unsup loss to outer loss

--- MetaBYOL/model/maml.py
+++ MetaBYOL/model/maml.py
@@ -48,7 +48,7 @@ class MAML:
               ):
 
         # Generate summary writer
-        meta_train_loss, meta_train_accuracy, meta_val_loss, meta_val_accuracy = define_metrics()
+        meta_train_loss, meta_train_accuracy, meta_val_loss, meta_val_accuracy, pre_update_train_accuracy = define_metrics()
         writer = tf.summary.create_file_writer(os.path.dirname(run_paths['path_logs_train']))
         logging.info(f"Saving log to {os.path.dirname(run_paths['path_logs_train'])}")
 
@@ -104,7 +104,7 @@ class MAML:
             for train1_ep, train2_ep, test_ep, test_label_ep in ds_train:
                 start = time.time()
                 self.meta_train_step(train1_ep, train2_ep, test_ep, test_label_ep, meta_optimizer,
-                                     meta_batch_size, meta_train_accuracy, meta_train_loss)
+                                     meta_batch_size, meta_train_accuracy, meta_train_loss, pre_update_train_accuracy)
 
                 stop = time.time()
                 # logging.info(f"Time for one iteration {stop - start}")
@@ -117,10 +117,13 @@ class MAML:
                 ckpt_manager.save(checkpoint_number=epoch)
 
             logging.info(f"Epoch {epoch}: acc = {meta_train_accuracy.result()}")
+            logging.info(f"Epoch {epoch}: pre_update_acc = {pre_update_train_accuracy.result()}")
             logging.info(f"Epoch {epoch}: val_acc = {meta_val_accuracy.result()}")
             logging.info(f"Epoch {epoch}: loss = {meta_train_loss.result()}")
             with writer.as_default():
                 tf.summary.scalar('Average meta test tasks accuracy', meta_train_accuracy.result(),
+                                  step=epoch)
+                tf.summary.scalar('Average pre update test tasks accuracy', pre_update_train_accuracy.result(),
                                   step=epoch)
                 tf.summary.scalar('Average meta test tasks loss', meta_train_loss.result(),
                                   step=epoch)
@@ -131,7 +134,7 @@ class MAML:
                 tf.summary.scalar('Learning rate', learning_rate_schedule(meta_optimizer.iterations),
                                   step=epoch)
 
-                reset_metrics(meta_train_loss, meta_train_accuracy, meta_val_loss, meta_val_accuracy)
+                reset_metrics(meta_train_loss, meta_train_accuracy, meta_val_loss, meta_val_accuracy, pre_update_train_accuracy)
         logging.info(f"Finished")
 
         return self.target_model
@@ -139,7 +142,7 @@ class MAML:
     @tf.function
     def func_help(self, train1_ep, train2_ep, test_ep, test_label_ep, meta_batch_size):
 
-        tasks_final_losses, test_predictions = tf.map_fn(
+        tasks_final_losses, test_predictions, meta_predictions = tf.map_fn(
             self.get_losses_of_tasks_batch,
             elems=(
                 train1_ep,
@@ -147,16 +150,16 @@ class MAML:
                 test_ep,
                 test_label_ep
             ),
-            dtype=(tf.float32, tf.float32),
+            dtype=(tf.float32, tf.float32, tf.float32),
             parallel_iterations=meta_batch_size
         )
         final_loss = tf.reduce_mean(tasks_final_losses)
-        return final_loss, test_predictions
+        return final_loss, test_predictions, meta_predictions
 
     def meta_train_step(self, train1_ep, train2_ep, test_ep, test_label_ep, meta_optimizer,
-                        meta_batch_size, meta_train_acc, meta_train_loss):
+                        meta_batch_size, meta_train_acc, meta_train_loss, pre_update_train_accuracy):
         with tf.GradientTape(persistent=False) as outer_tape:
-            final_loss, test_predictions = self.func_help(train1_ep, train2_ep, test_ep, test_label_ep, meta_batch_size)
+            final_loss, test_predictions, meta_predictions = self.func_help(train1_ep, train2_ep, test_ep, test_label_ep, meta_batch_size)
         outer_gradients = outer_tape.gradient(final_loss, self.target_model.trainable_variables)
         # Apply some clipping
         #outer_gradients = [tf.clip_by_norm(g, 10.0)
@@ -164,16 +167,32 @@ class MAML:
 
         meta_optimizer.apply_gradients(zip(outer_gradients, self.target_model.trainable_variables))
         meta_train_acc(test_label_ep, test_predictions)
+        pre_update_train_accuracy(test_label_ep, meta_predictions)
         meta_train_loss(final_loss)
+
 
     # @tf.function
     def get_losses_of_tasks_batch(self, inputs):
         train1_ep, train2_ep, test_ep, test_label_ep = inputs
         updated_model = self.inner_train_loop(train1_ep, train2_ep)
+        meta_prediction = self.target_model(test_ep, training=True,
+                                        unsupervised_training=False)
         test_prediction = updated_model(test_ep, training=True,
-                                        unsupervised_training=False)  # TODO: training=True or False??
+                                        unsupervised_training=False)
         test_loss = tf.reduce_mean(self.loss_function(test_label_ep, test_prediction))
-        return test_loss, test_prediction
+        tar1 = updated_model(train1_ep, training=True, unsupervised_training=True)
+        tar2 = updated_model(train2_ep, training=True,
+                                          unsupervised_training=True)
+        prediction1 = updated_model(train1_ep, training=True, unsupervised_training=True,
+                                                 online=True)
+        prediction2 = updated_model(train2_ep, training=True, unsupervised_training=True,
+                                                 online=True)
+        loss1 = self.byol_loss_fn(prediction1, tf.stop_gradient(tar2))
+        loss2 = self.byol_loss_fn(prediction2, tf.stop_gradient(tar1))
+        loss = tf.reduce_mean(loss1 + loss2)
+        test_loss = test_loss+loss        
+
+        return test_loss, test_prediction, meta_prediction
 
     def create_meta_model(self, updated_model, model, gradients):
         k = 0
@@ -242,7 +261,6 @@ class MAML:
         self.create_meta_model(self.updated_models[0], self.target_model,
                                gradients)
 
-
         for k in range(1, self.num_steps_ml + 1):
             tar1 = self.updated_models[k - 1](train1_ep, training=True, unsupervised_training=True)
             tar2 = self.updated_models[k - 1](train2_ep, training=True,
@@ -259,6 +277,7 @@ class MAML:
             gradients = train_tape.gradient(loss, self.updated_models[k - 1].meta_trainable_variables)
             #tf.print(gradients)
             self.create_meta_model(self.updated_models[k], self.updated_models[k - 1], gradients)
+
 
         return self.updated_models[-1]
 
@@ -299,10 +318,11 @@ def define_metrics():
     val_loss = tf.keras.metrics.Mean(name='val_loss')
     train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='meta_train_accuracy')
     val_accuracy = tf.keras.metrics.CategoricalAccuracy(name='val_accuracy')
-    return train_loss, train_accuracy, val_loss, val_accuracy
+    pre_update_train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='pre_update_train_accuracy')
+    return train_loss, train_accuracy, val_loss, val_accuracy, pre_update_train_accuracy
 
 
-def reset_metrics(train_loss, train_accuracy, val_loss, val_accuracy):
+def reset_metrics(train_loss, train_accuracy, val_loss, val_accuracy, pre_update_train_accuracy):
     """
     This function resets metrics after epoch during training.
     Args:
@@ -316,4 +336,5 @@ def reset_metrics(train_loss, train_accuracy, val_loss, val_accuracy):
     train_accuracy.reset_states()
     val_loss.reset_states()
     val_accuracy.reset_states()
+    pre_update_train_accuracy.reset_states()
 
